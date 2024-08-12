9fe445441461b837ce8dfe1e8e6228d2d8b721aa
Shantanu Inagle <shantanuingale2002@gmail.com>
2024-08-12 12:03:25 +0530

    connect to mongodb

--- auth-project-mern-slp/api/index.js
+++ auth-project-mern-slp/api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+// db connect
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDB...");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// app init
 const app = express();
 
+// app listen
 app.listen(3000, () => {
     console.log("Server listening on localhost:3000...");
 });
