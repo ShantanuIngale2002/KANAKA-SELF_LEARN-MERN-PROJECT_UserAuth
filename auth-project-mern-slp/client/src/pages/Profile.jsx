ebe26b3369ef68e67792d52b021fefc414e6c368
Shantanu Inagle <shantanuingale2002@gmail.com>
2024-08-20 16:01:49 +0530

    console changes in below files

--- auth-project-mern-slp/client/src/pages/Profile.jsx
+++ auth-project-mern-slp/client/src/pages/Profile.jsx
@@ -1,16 +1,66 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { firebaseApp } from "../firebase";
 
 const Profile = () => {
+    const fileRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
+
+    const [image, setImage] = useState(undefined);
+    const [imagePercent, setImagePercent] = useState(0);
+    const [imageError, setImageError] = useState(false);
+    const [formData, setFormData] = useState({});
+
+    // useEffect(() => {
+    //     handleImageUpdate(image);
+    // }, [image]);
+
+    const handleImageUpdate = (image) => {
+        const storage = getStorage(firebaseApp); // firebase storage service at config firebase.js's firebaseApp
+        const fileName = new Date().getTime() + image?.name; // always unique
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        // uploading
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setImagePercent(Math.round(progress)); // set := progress of image uploading
+            },
+            (error) => setImageError(true),
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setFormData({ ...formData, profilePicture: downloadURL });
+                });
+            }
+        );
+    };
+
+    //console.log(formData);
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
             <form className="flex flex-col gap-4">
+                <input
+                    type="file"
+                    ref={fileRef}
+                    accept="image/*"
+                    hidden
+                    onChange={(e) => handleImageUpdate(e.target.files[0])}
+                />
                 <img
                     src={currentUser.profilePicture}
                     alt="PP"
                     className="h-24 w-24 self-center rounded-full cursor-pointer object-cover mt-2"
+                    onClick={() => fileRef.current.click()}
                 />
                 <input
                     defaultValue={currentUser.username}
@@ -36,7 +86,7 @@ const Profile = () => {
                     Update
                 </button>
             </form>
-            <div className="flex justify-between mt-5">
+            <div className="flex justify-between my-5 mx-2">
                 <span className="text-red-700 cursor-pointer">
                     Delete Account
                 </span>
